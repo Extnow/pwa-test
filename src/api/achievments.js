ade36d0f51067919dca6479a5cb3ab115a99dae6
extnow <ryabushkin.mikhail@gmail.com>
2019-03-22 11:04:45 +0300

    добавил картинку медали в проект, оптимизировал ее

--- src/api/achievments.js
+++ src/api/achievments.js
@@ -1,8 +1,10 @@
+import medal from './medal.png';
+
 const achievements = [
   {
     id: 0,
     isDone: false,
-    img: 'http://gimnazist1.ru/files/image/awards/sport_badges-02-512.png',
+    img: medal,
     text: 'Ответьте правильно на все вопросы',
   },
 ];
